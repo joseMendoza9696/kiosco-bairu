b3180848a55da15ee74b48ff60598a31d86505d9
lfernand_cs <fwzk777@gmail.com>
2024-03-23 19:51:30 -0400

    cambios vista bienvenida

--- src/pages/Bienvenida.tsx
+++ src/pages/Bienvenida.tsx
@@ -22,30 +22,50 @@ export const Bienvenida = () => {
 
   return (
     <div className="container mx-auto mt-8 p-4">
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center pb-40 pt-10">
         <img
           src={profileData?.contextStyle.logo}
           alt="logo"
-          className="rounded-lg h-80 w-80 mr-4"
+          className="rounded-full h-40 w-40 mr-4"
         />
       </div>
       <div>
-        <h1 className="text-5xl font-bold text-dark text-center">
-          ORDENA AQUÍ
+        <h1 className=" text-4xl lg:text-9xl font-bold text-dark text-center ">
+          LA VIDA ES MÁS FÁCIL PIDIENDO ASÍ
+          <span className="p-10">
+            <Icon
+              icon="mdi:arrow-down-bold"
+              style={{
+                height: '190px',
+                width: ' 190px',
+                margin: '10 auto',
+              }}
+            />
+          </span>
         </h1>
       </div>
-      <div className="flex justify-center mt-8">
-        <button className="mx-4 px-4 py-2 btn btn-outline btn-secondary">
+      <div className="flex justify-center mt-8 space-x-20 ">
+        <button className="mx-4 px-6 py-3 btn btn-primary btn-secondary text-2xl w-72 h-72 flex flex-col items-center justify-center rounded-3xl">
           <a href="/menu">
-            <Icon icon="zondicons:location-food" />
-            Comer aquí
+            <div className="flex flex-col items-center">
+              <Icon icon="zondicons:location-food" className="text-8xl mb-6" />
+              <span className="text-3xl">Comer aquí</span>
+            </div>
           </a>
         </button>
-        <button className="mx-4 px-4 py-2 btn btn-outline btn-secondary">
-          <Icon icon="fa6-solid:basket-shopping" />
-          Para llevar
+        <button className="mx-4 px-6 py-3 btn btn-primary btn-secondary text-2xl w-72 h-72 flex flex-col items-center justify-center rounded-3xl">
+          <a href="/para-llevar">
+            <div className="flex flex-col items-center">
+              <Icon
+                icon="fa6-solid:basket-shopping"
+                className="text-8xl mb-6"
+              />
+              <span className="text-3xl">Para llevar</span>
+            </div>
+          </a>
         </button>
       </div>
+
       <Footer />
     </div>
   );
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,11 +1,9 @@
-// 1080 1920
-
 export const Footer = () => {
   return (
-    <footer className="footer footer-center p-4  text-base-content text-2xl	">
-      <aside>
+    <footer className="fixed bottom-0 w-full  p-8 text-5xl text-center">
+      <div className="container mx-auto">
         <h1>Powered by Bairü</h1>
-      </aside>
+      </div>
     </footer>
   );
 };
