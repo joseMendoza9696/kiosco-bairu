797244a1a0667fcd33d2fa2afc4cd4a86dd1ba5c
lfernand_cs <fwzk777@gmail.com>
2024-03-31 23:27:57 -0400

    Add @reduxjs/toolkit dependency and font-face declaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 
   plugins: [require('daisyui')],
+
   daisyui: {
+    fontFamily: {
+      custom: ['font-bairu', 'sans-serif'],
+    },
     themes: [
       {
         mytheme: {
