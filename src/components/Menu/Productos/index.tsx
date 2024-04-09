cf22071046b5582eebd16b528c841f7ae95ed245
lfernand_cs <fwzk777@gmail.com>
2024-04-09 19:20:51 -0400

    orden de componentes y arreglo de redux reload

--- src/components/Menu/Productos/index.tsx
+++ src/components/Menu/Productos/index.tsx
@@ -1,4 +1,79 @@
-// contiene de productos tsx i reemplaza al produtctos tsx
-// contiene el popup de productos
-// solucionar el bug
-// redux necesita un tiempo para cargar los datos
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+
+const Productos = () => {
+  // contiene de productos tsx i reemplaza al produtctos tsx CHECK
+  // contiene el popup de productos
+  // solucionar el bug CHECK
+  // redux necesita un tiempo para cargar los datos CHECK
+  // cuando entre al array del menu
+  const categoriaSeleccionada = useSelector(
+    (state: RootState) => state.menuSeleccionReducer.categoriaSeleccionada,
+  );
+
+  const categoriaActual = useSelector(
+    (state: RootState) => state.menuReducer.categorias[categoriaSeleccionada],
+  );
+
+  if (
+    !categoriaActual ||
+    !categoriaActual.subcategorias ||
+    categoriaActual.subcategorias.length === 0
+  ) {
+    return <div>No hay productos disponibles</div>;
+  }
+
+  const productos = categoriaActual.subcategorias[0].productos;
+
+  return (
+    <div className="pt-10 text-center ">
+      <div className="flex items-center justify-between pl-16">
+        <h1 className="text-4xl font-semibold text-primary">
+          Escoge tu producto
+        </h1>
+
+        <button className="btn btn-secondary w-16 h-16 btn-circle mr-16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="flex flex-wrap mx-[64px]">
+        {productos.map((producto) => (
+          <button
+            key={producto.id}
+            className="flex flex-col mr-[32px] py-8 rounded-md shadow-md  "
+          >
+            <img
+              src={producto.imagen}
+              alt={producto.nombre}
+              className="w-[285px] h-[285px] rounded-xl object-cover"
+            />
+            <div className="ml-2">
+              <h2 className="text-[20px] font-semibold text-left ">
+                {producto.nombre}
+              </h2>
+              <p className="text-left text-semibold text-lg">
+                Bs. {producto.precio}
+              </p>
+              <p className="text-left">{producto.descripcion}</p>
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Productos;
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,7 +5,7 @@ import { guardarMenu } from '../../redux/actions/menu.action';
 import { useLazyQuery } from '@apollo/client';
 import Categoria from './categorias';
 import { RootState } from '../../redux/store';
-import Productos from './productos';
+import Productos from './Productos/index.tsx';
 
 export const Menu = () => {
   const dispatch = useDispatch();
@@ -41,38 +41,17 @@ export const Menu = () => {
         </div>
       </header>
       <main>
-        {/*TODO: MOVER LAS CATEGORIAS AL COMPONENTE CATEGORIAS.TSX*/}
+        {/* CATEGORIAS.TSX*/}
 
         <Categoria categorias={categorias} />
 
-        {/*TODO: CATEGORIAS.TSX END*/}
+        {/*CATEGORIAS.TSX END*/}
 
-        {/*TODO: MOVER LOS PRODUCTOS AL COMPONENTE PRODUCTOS.TSX*/}
-        <div className="pt-10 text-center ">
-          <div className="flex items-center justify-between pl-16">
-            <h1 className="text-4xl font-semibold text-primary">
-              Escoge tu producto
-            </h1>
+        {/*PRODUCTOS.TSX*/}
 
-            <button className="btn btn-secondary w-16 h-16 btn-circle mr-16">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </div>
-          <Productos />
-        </div>
+        <Productos />
+
+        {/*PRODUCTOS.TSX END*/}
       </main>
     </>
   );
