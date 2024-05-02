326ec959600f8eac57bfa6ec984580ba376a1286
lfernand_cs <fwzk777@gmail.com>
2024-05-01 23:40:16 -0400

    checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -31,59 +31,94 @@ export const Checkout = () => {
   console.log(tipoEntrega);
 
   return (
-    <div className=" text-center pt-[80px]">
-      <h1 className="  text-primary font-bold text-[48px]">TU ORDEN</h1>
-
-      {/* SECCION DEL TOOGLE */}
-      <label
-        htmlFor="Toggle3"
-        className="inline-flex items-center p-2  cursor-pointer rounded-xl w-[-840px] text-[24px] text-white h-[96px]"
-      >
-        <input
-          id="Toggle3"
-          type="checkbox"
-          className="hidden peer"
-          value={tipoEntrega}
-          onChange={cambioTipoEntrega}
-        />
-        <span className="px-4 py-2 rounded-l-md bg-primary peer-checked:bg-white">
-          Comer Aquí
-        </span>
-        <span className="px-4 py-2 rounded-r-md bg-white peer-checked:bg-primary">
-          Para llevar
-        </span>
-      </label>
-
-      {/* SECCION DE PRODUCTOS */}
-
-      <div className="mt-8">
-        <ul className="space-y-4">
-          {productosSeleccionados.map((producto, index) => (
-            <li key={index} className="flex items-center gap-4">
-              <img
-                src={producto.imagen}
-                alt={producto.nombre}
-                className="size-16 rounded object-cover"
+    <div className=" w-full ">
+      <div className="max-w-screen-xl w-11/2 m-auto">
+        <h1 className="text-center text-primary font-bold text-[56px] pt-[116px] pb-[60px]">
+          TU ORDEN
+        </h1>
+        {/* SECCION DEL TOOGLE */}
+        <div className=" w-full ">
+          <div className="max-w-screen-xl w-11/2 m-auto ">
+            <label
+              htmlFor="Toggle3"
+              className=" items-center  flex justify-center cursor-pointer rounded-xl  text-[24px] h-[96px]"
+            >
+              <input
+                id="Toggle3"
+                type="checkbox"
+                className="hidden peer"
+                value={tipoEntrega}
+                onChange={cambioTipoEntrega}
               />
+              <span className="  rounded-l-2xl bg-primary peer-checked:bg-white w-1/3 py-[35px] text-white peer-checked:text-primary text-[32px] text-center">
+                Comer Aquí
+              </span>
+              <span className=" rounded-r-2xl  text-center bg-white peer-checked:bg-primary w-1/3 py-[35px]  text-primary peer-checked:text-white text-[32px]">
+                Para llevar
+              </span>
+            </label>
+          </div>
+        </div>
 
-              <div>
-                <h3 className="text-sm text-gray-900">{producto.nombre}</h3>
-
-                <dl className="mt-0.5 space-y-px text-[10px] text-gray-600"></dl>
+        {/* SECCION DE PRODUCTOS */}
+        {productosSeleccionados.map((producto, index) => (
+          <div className=" w-full " key={index}>
+            <div className="flex justify-center ">
+              <div className="flex justify-between w-[91%] my-10  py-10 px-8 shadow-xl rounded-2xl ">
+                <div className="flex w-[550px] space-between gap-5">
+                  <div>
+                    <img
+                      src={producto.imagen}
+                      className="w-[200px] h-[200px] rounded-[20px] object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between ">
+                    <h1 className="text-primary text-[30px] font-bold">
+                      {producto.nombre}
+                    </h1>
+                    <button className=" btn btn-gosth text-[24px] font-bold">
+                      {' '}
+                      Descripcion del producto
+                    </button>
+                  </div>
+                </div>
+                <div className="text-[40px] flex flex-col justify-between">
+                  <div className="flex justify-end">
+                    <p>Bs. {producto.precioOriginal}</p>
+                  </div>
+                  <div className="flex w-[300px] justify-between items-center">
+                    <button className="w-[70px] h-[70px] items-center border-4 rounded-full border-zinc-950 flex justify-center ">
+                      x
+                    </button>
+                    <button className="w-[70px] h-[70px] items-center border-4 rounded-full border-zinc-950 flex justify-center ">
+                      -
+                    </button>
+                    <button className="w-[70px] h-[70px] items-center   flex justify-center ">
+                      1
+                    </button>
+                    <button className="w-[70px] h-[70px] items-center border-4 rounded-full border-zinc-950 flex justify-center ">
+                      +
+                    </button>
+                  </div>
+                </div>
               </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+          </div>
+        ))}
+        {/* SECCION DE TOTAL */}
+        <h1 className="text-center text-primary font-bold text-[56px] pt-[116px] pb-[60px]">
+          TOTAL {productosSeleccionados[0].precioTotal}
+        </h1>
 
-      {/* SECCION DE BOTONES */}
-      <div className="text-center my-[127px] space-x-[100px] ">
-        <button className="btn btn-gosth w-[329px] h-[190px] text-[30px] rounded-[20px] mb-16">
-          <Link to="/menu">Cancelar</Link>
-        </button>
-        <button className="btn btn-primary w-[329px] h-[190px] text-[30px] rounded-[20px] mb-16">
-          Ir a pagar
-        </button>
+        {/* SECCION DE BOTONES */}
+        <div className="text-center my-[127px] space-x-[100px] ">
+          <button className="btn btn-gosth w-[329px] h-[190px] text-[30px] rounded-[20px] mb-16">
+            <Link to="/menu">Cancelar</Link>
+          </button>
+          <button className="btn btn-primary w-[329px] h-[190px] text-[30px] rounded-[20px] mb-16">
+            Ir a pagar
+          </button>
+        </div>
       </div>
     </div>
   );
