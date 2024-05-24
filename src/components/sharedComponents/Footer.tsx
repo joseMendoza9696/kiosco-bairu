0f3f9be8738c8b72be0d96f9010c403f784d4411
lfernand_cs <fwzk777@gmail.com>
2024-05-23 22:37:58 -0400

    arreglo de bugs y esteticos

--- src/components/sharedComponents/Footer.tsx
+++ src/components/sharedComponents/Footer.tsx
@@ -8,19 +8,25 @@ export const Footer = () => {
 
   if (nuevaOrden.productos.length > 0) {
     return (
-      <footer className="fixed bottom-0 w-full text-[30px] text-center py-4  bg-white rounded-t-[60px] border-8 border-primary border-b-0   ">
-        <div className="container mx-auto  flex justify-center items-center bg-white rounded-3xl">
-          <div className="h-[242px] font-bold flex justify-center items-center space-x-8 gap-x-9">
-            <span className="  text-[60px]">Bs. {nuevaOrden.cuentaTotal} </span>
+      <footer className="fixed bottom-0 w-full text-[30px] text-center py-4  bg-white rounded-t-[60px] border-8 border-primary border-b-0 ">
+        <button>
+          <Link to="/checkout">
+            <div className="container mx-auto  flex justify-center items-center bg-white rounded-3xl">
+              <div className="h-[242px] font-bold flex justify-center items-center space-x-8 gap-x-9">
+                <span className="  text-[60px]">
+                  Bs. {nuevaOrden.cuentaTotal}{' '}
+                </span>
 
-            <span className="text-[50px] text-gray-500">
-              Productos: {nuevaOrden.productos.length}{' '}
-            </span>
-            <button className="btn btn-primary w-[330px] h-[80px] text-[40px] rounded-[20px]  ">
-              <Link to="/checkout">Ver pedido</Link>
-            </button>
-          </div>
-        </div>
+                <span className="text-[50px] text-gray-500">
+                  Productos: {nuevaOrden.productos.length}{' '}
+                </span>
+                <button className="btn btn-primary w-[330px] h-[80px] text-[40px] rounded-[20px]  ">
+                  <Link to="/checkout">Ver pedido</Link>
+                </button>
+              </div>
+            </div>
+          </Link>
+        </button>
       </footer>
     );
   }
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -18,6 +18,17 @@ export const Checkout = () => {
   const navigate = useNavigate();
 
   const nuevaOrden = useSelector((state: RootState) => state.nuevaOrdenReducer);
+  const categorias = useSelector(
+    (state: RootState) => state.menuReducer.categorias,
+  );
+  console.log(categorias);
+
+  const categoriaSeleccionada = useSelector(
+    (state: RootState) => state.menuSeleccionReducer.categoriaSeleccionada,
+  );
+  const categoriaActual = useSelector(
+    (state: RootState) => state.menuReducer.categorias[categoriaSeleccionada],
+  );
 
   const [tipoEntrega, setTipoEntrega] = useState<string>('AQUI');
 
@@ -49,12 +60,11 @@ export const Checkout = () => {
         <h1 className="text-center text-primary font-bold text-[56px] pt-[116px] pb-[60px]">
           TU ORDEN
         </h1>
-        {/* SECCION DEL TOOGLE */}
-        <div className=" w-full pb-8">
+        <div className=" w-full pb-8 ">
           <div className="max-w-screen-xl w-11/2 m-auto ">
             <label
               htmlFor="Toggle3"
-              className=" items-center  flex justify-center cursor-pointer rounded-xl  text-[24px] h-[96px] "
+              className=" items-center  flex justify-center cursor-pointer rounded-2xl  text-[24px] h-[96px] bg-primary mx-[124px] "
             >
               <input
                 id="Toggle3"
@@ -63,96 +73,100 @@ export const Checkout = () => {
                 value={tipoEntrega}
                 onChange={cambioTipoEntrega}
               />
-              <span className="  rounded-l-2xl bg-primary peer-checked:bg-white w-1/3 py-[35px] text-white peer-checked:text-primary text-[32px] text-center">
+              <span className="rounded-2xl bg-primary peer-checked:bg-white w-1/2 mx-4 h-[70px] font-semibold text-white peer-checked:text-primary text-[35px] text-center flex items-center justify-center">
                 Comer Aquí
               </span>
-              <span className=" rounded-r-2xl  text-center bg-white peer-checked:bg-primary w-1/3 py-[35px]  text-primary peer-checked:text-white text-[32px]">
+              <span className=" rounded-2xl h-[70px] text-center font-semibold bg-white peer-checked:bg-primary w-1/2 mx-4 text-primary peer-checked:text-white text-[35px] flex items-center justify-center">
                 Para llevar
               </span>
             </label>
           </div>
         </div>
 
-        {/* SECCION DE PRODUCTOS */}
-        <div className="flex flex-wrap mx-[56px]  overflow-auto overflow-y-auto max-h-[1000px]">
-          {nuevaOrden.productos.map((producto, index) => (
-            <div className="" key={index}>
-              <div className="flex justify-center ">
-                <div className="flex justify-between w-[91%] my-10  py-10 px-8 shadow-xl rounded-2xl border-2">
-                  <div className="flex w-[550px] space-between gap-5">
-                    <div>
-                      <img
-                        src={producto.imagen}
-                        className="w-[200px] h-[200px] rounded-[20px] object-cover"
-                      />
-                    </div>
-                    <div className="flex flex-col justify-between ">
-                      <h1 className="text-primary text-[30px] font-bold">
-                        {producto.nombre}
-                      </h1>
-                      {/*<button className=" btn btn-gosth text-[24px] font-bold">*/}
-                      {/*  {producto.}*/}
-                      {/*</button>*/}
-                    </div>
-                  </div>
-                  <div className="text-[40px] flex flex-col justify-between">
-                    <div className="flex justify-end">
-                      <p>Bs. {producto.precioTotal}</p>
-                    </div>
-                    <div className="flex w-[300px] justify-between items-center">
-                      <button
-                        className="w-[70px] h-[70px] items-center  rounded-full flex justify-center "
-                        onClick={() => {
-                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                          // @ts-expect-error
-                          dispatch(eliminarProducto(index));
-                          if (index === 0) {
-                            navigate('/menu');
-                          }
-                        }}
-                      >
-                        <Icon
-                          icon="ic:outline-cancel"
-                          className="w-full h-full flex items-center justify-center"
+        <div className="flex flex-wrap mx-[56px]  overflow-auto overflow-y-auto max-h-[1000px] scroll-hidden">
+          {categoriaActual &&
+            nuevaOrden.productos.map((producto, index) => (
+              <div key={index}>
+                <div className="flex justify-center ">
+                  <div className="flex justify-between w-[98%] my-4  py-6 px-16 shadow-xl rounded-[40px] border-2 bg-white">
+                    <div className="flex w-[550px] space-between gap-5 ">
+                      <div>
+                        <img
+                          src={producto.imagen}
+                          className="w-[200px] h-[200px] rounded-[20px] object-cover"
                         />
-                      </button>
-                      <button
-                        className="w-[70px] h-[70px] items-center  rounded-full  flex justify-center "
-                        onClick={() => {
-                          editarCantidad(index, -1);
-                        }}
-                      >
-                        <Icon
-                          icon="zondicons:minus-outline"
-                          className="w-full h-full flex items-center justify-center"
-                        />
-                      </button>
-                      <button className="w-[70px] h-[70px] items-center   flex justify-center ">
-                        {producto.cantidad}
-                      </button>
-                      <button
-                        className="w-[70px] h-[70px] items-center  flex justify-center "
-                        onClick={() => {
-                          editarCantidad(index, 1);
-                        }}
-                      >
-                        <Icon
-                          icon="zondicons:add-outline"
-                          className="w-full h-full flex items-center justify-center"
-                        />
-                      </button>
+                      </div>
+                      <div className="flex flex-col justify-between ">
+                        <h1 className=" text-[40px] font-semibold">
+                          {producto.nombre}
+                          <p className="text-[28px] font-bold text-primary">
+                            {categoriaActual?.nombre || 'No disponible'}
+                          </p>
+                        </h1>
+
+                        {/* <button className=" btn btn-gosth text-[24px] font-bold">
+                          Modificar{' '}
+                        </button> */}
+                      </div>
+                    </div>
+                    <div className="text-[40px] flex flex-col justify-between">
+                      <div className="flex justify-end font-bold">
+                        <p>Bs. {producto.precioTotal}</p>
+                      </div>
+                      <div className="flex w-[300px] justify-between items-center">
+                        <button
+                          className="w-[70px] h-[70px] items-center  rounded-full flex justify-center "
+                          onClick={() => {
+                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            // @ts-expect-error
+                            dispatch(eliminarProducto(index));
+                            if (index === 0) {
+                              navigate('/menu');
+                            }
+                          }}
+                        >
+                          <Icon
+                            icon="ic:outline-cancel"
+                            className="w-full h-full flex items-center justify-center"
+                          />
+                        </button>
+                        <button
+                          className="w-[70px] h-[70px] items-center  rounded-full  flex justify-center mx-2"
+                          onClick={() => {
+                            editarCantidad(index, -1);
+                          }}
+                        >
+                          <Icon
+                            icon="zondicons:minus-outline"
+                            className="w-full h-full flex items-center justify-center "
+                          />
+                        </button>
+                        <button className="w-[70px] h-[70px] items-center   flex justify-center mx-2">
+                          {producto.cantidad}
+                        </button>
+                        <button
+                          className="w-[70px] h-[70px] items-center  flex justify-center mx-2"
+                          onClick={() => {
+                            editarCantidad(index, 1);
+                          }}
+                        >
+                          <Icon
+                            icon="zondicons:add-outline"
+                            className="w-full h-full flex items-center justify-center "
+                          />
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         {/* SECCION DE TOTAL */}
 
         {/* SECCION DE BOTONES */}
-        <div className="fixed bottom-0 left-0 right-0">
-          <h1 className="text-center text-primary font-bold text-[56px]  ">
+        <div className="fixed bottom-0 left-0 right-0 ">
+          <h1 className="text-center text-primary font-bold text-[56px] ">
             Total Bs.{nuevaOrden.cuentaTotal}
           </h1>
           <div className="text-center my-[100px]  flex justify-between mx-40 ">
