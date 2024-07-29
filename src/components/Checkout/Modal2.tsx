385bda1558d8459826db26000c0ca16e414eb9ae
Emmanuel Canqui Pinto <emmanuel.marthin.c@gmail.com>
2024-07-29 16:55:08 -0400

    se copio las propiedades tailwind a Checkout modal2

--- src/components/Checkout/Modal2.tsx
+++ src/components/Checkout/Modal2.tsx
@@ -80,7 +80,7 @@ export const Modal2 = () => {
   const storageNotes = perfilLocalStorage.notas_productos;
   return (
     <dialog id="checkout2" className="modal modal-bottom">
-      <div className="relative modal-box bg-base-100 rounded-t-[5.6rem] md:h-[93%] lg:h-[87%] md:px-10 md:pt-24 lg:px-24 lg:pt-44">
+      <div className="relative modal-box bg-base-100 rounded-t-[5.6rem] md:h-[93%] lg:h-[87%] md:px-8 md:pt-24 lg:px-20 lg:pt-36">
         {/* button close modal */}
         <form method="dialog">
           <button
@@ -96,7 +96,7 @@ export const Modal2 = () => {
           </button>
         </form>
         {/* product image */}
-        <div className="object-cover md:h-[16rem] lg:h-[25rem] md:mb-4 lg:mb-10">
+        <div className="object-cover md:h-[18rem] lg:h-[25rem] md:mb-2 lg:mb-4">
           <img
             src={editOrder.imagen}
             alt={editOrder.nombre}
@@ -104,7 +104,7 @@ export const Modal2 = () => {
           />
         </div>
         {/* name product and notes */}
-        <div className="flex gap-6 justify-center md:items-center md:mb-4 lg:mb-8">
+        <div className="flex gap-6 justify-center md:items-center md:mb-2 lg:mb-4">
           <h2 className="font-bold md:text-4xl lg:text-6xl text-center">
             {editOrder.nombre}
           </h2>
@@ -112,17 +112,17 @@ export const Modal2 = () => {
           {storageNotes && <NotesProduct.Modal2 />}
         </div>
         {/* description */}
-        <p className="md:text-xl lg:text-3xl text-center text-[#A6A6AA] md:mb-2 lg:mb-8">
+        <p className="md:text-xl lg:text-3xl text-center text-[#A6A6AA] md:mb-2 lg:mb-4">
           {editOrder.descripcion}
         </p>
         {/* price */}
-        <span className="block text-center text-primary md:text-4xl lg:text-5xl font-semibold md:mb-4">
+        <span className="block text-center text-primary md:text-4xl lg:text-5xl font-semibold md:mb-2 lg:mb-4">
           {currencyLocal}{' '}
           {parseFloat(editOrder.precioTotal.toString()).toFixed(2)}
         </span>
         {/* component steps */}
-        <div className="bg-[#F2F2F2] md:p-4 lg:p-8 rounded-3xl mb-5 text-start">
-          <ul className="steps w-full md:mb-3">
+        <div className="bg-[#F2F2F2] rounded-3xl  text-start md:mb-2 lg:mb-4 md:p-4 lg:p-8 ">
+          <ul className="steps w-full md:mb-1 lg:mb-4">
             {editOrder.opcionesMenu.map(
               (_option: OpcionMenuNuevaOrden, i: number) => (
                 <li
@@ -143,7 +143,21 @@ export const Modal2 = () => {
           </p>
         </div>
         {/* OPTIONS PRODUCT */}
-        <div className="overflow-y-auto scroll-hidden grid md:grid-cols-3 lg:grid-cols-4 justify-items-center md:max-h-[22rem] lg:max-h-[24rem]">
+        <div
+          className={`overflow-y-auto scroll-hidden grid md:grid-cols-3 lg:grid-cols-4 justify-items-center 
+            ${
+              editOrder.nombre.length > 18 && editOrder.descripcion.length > 140
+                ? 'md:max-h-[23rem] lg:max-h-[30rem]'
+                : editOrder.nombre.length > 25
+                  ? 'md:max-h-[27rem] lg:max-h-[35rem]'
+                  : !editOrder.descripcion.length &&
+                      editOrder.nombre.length > 21
+                    ? 'md:max-h-[28rem] lg:max-h-[34rem]'
+                    : !editOrder.descripcion.length
+                      ? 'md:max-h-[26rem] lg:max-h-[32rem]'
+                      : 'md:max-h-[25rem] lg:max-h-[29rem]'
+            }`}
+        >
           {/*  PRODUCT ITEMS */}
           {optionsProduct &&
             optionsProduct.map((option: Opcion, i: number) => (
@@ -184,7 +198,7 @@ export const Modal2 = () => {
             ))}
         </div>
         {/* BUTTONS cancel minus number plus procedd */}
-        <div className="md:bottom-8 lg:bottom-9 flex justify-between items-center absolute md:w-[90%] lg:w-[82%]">
+        <div className="md:bottom-5 lg:bottom-9 flex justify-between items-center absolute md:w-[90%] lg:w-[85%] bg-base-100">
           {/* BACK, CANCEL */}
           <button
             className="box-content btn font-semibold md:text-2xl lg:text-4xl rounded-3xl md:py-[1.2em] md:w-[5.5em] lg:py-[1.5em] lg:w-[5.4em]"
@@ -202,7 +216,7 @@ export const Modal2 = () => {
             className="box-content h-max btn md:py-[0.2em] md:w-[1.5em] md:rounded-2xl lg:rounded-3xl md:text-5xl lg:text-6xl"
             disabled={editOrder.cantidad <= 1}
           >
-            <Icon width="48px" height="48px" icon="icomoon-free:minus" />
+            <Icon icon="icomoon-free:minus" />
           </button>
           {/* AMOUNT */}
           <span className="md:text-3xl lg:text-5xl font-bold">
@@ -217,12 +231,7 @@ export const Modal2 = () => {
               dispatch(modificarCantidadProducto(1))
             }
           >
-            <Icon
-              width="48px"
-              height="48px"
-              icon="icomoon-free:plus"
-              className=""
-            />
+            <Icon icon="icomoon-free:plus" className="" />
           </button>
           {/* ADD, CONTINUE */}
           <button
